ce250768b0ad3671177e65a98e4a881625fa50df
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-05-20 17:26:40 +0200

    23.1 Spring REST API II

--- src/main/java/com/crud/tasks/trello/config/TrelloConfig.java
+++ src/main/java/com/crud/tasks/trello/config/TrelloConfig.java
@@ -3,7 +3,6 @@ package com.crud.tasks.trello.config;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
